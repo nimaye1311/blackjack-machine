e6a7dc8b631fc2ea8c5407e749558f2bb3254584
Nimaye Garodia <nimaye@gmail.com>
2025-04-21 17:06:36 -0400

    Added modulo specific test

---
+++ proc-toolchain-student/test_files/assembly_files/mod.s
@@ -0,0 +1,18 @@
+nop
+addi $r1, $r0, 29        
+addi $r2, $r0, 1
+addi $r3, $r0, 2
+addi $r4, $r0, 3
+addi $r5, $r0, 4
+addi $r6, $r0, 5
+addi $r7, $r0, 6
+addi $r8, $r0, 8
+div $r9, $r1, $r2        
+div $r10, $r1, $r3        
+div $r11, $r1, $r4        
+div $r12, $r1, $r5        
+div $r13, $r1, $r6        
+div $r14, $r1, $r7 
+div $r15, $r1, $r8
+nop
+nop
